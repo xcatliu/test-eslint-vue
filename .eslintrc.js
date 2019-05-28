40740fa859cdd725c16fa2840a8cc6f82efc3598
xcatliu <xcatliu@gmail.com>
2019-05-28 15:22:47 +0800

    Test vue-eslint-parser v6

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  parser: 'vue-eslint-parser',
+  plugins: ['vue'],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+    'vue/valid-v-bind': 'error'
+  }
+}
